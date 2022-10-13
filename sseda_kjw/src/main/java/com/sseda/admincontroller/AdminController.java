3e7820c5666361ae2089b3d7eda7aa9008b4a5f8
user <user@user21>
2022-10-13 11:02:02 +0900

    cre 수정

--- sseda_kjw/src/main/java/com/sseda/admincontroller/AdminController.java
+++ sseda_kjw/src/main/java/com/sseda/admincontroller/AdminController.java
@@ -1,53 +1,33 @@
 package com.sseda.admincontroller;
 
 
-import javax.servlet.http.HttpSession;
-
 
 import org.slf4j.Logger;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import com.sseda.dto.Member;
-import com.sseda.service.AdminService;
 
-@Controller
-public class AdminController {
-	
-	private static final Logger log = org.slf4j.LoggerFactory.getLogger(AdminController.class);
-	
-	@Autowired
-	AdminService ad;
-	
-	@GetMapping("gologin")
-	public String m_login() {
-		log.info("admin home...........");
-		return "/ad_main/m_login";
-	}
 
-	
-	@PostMapping("adminlogin")
-	public String login(Member mem, HttpSession sess) {
-		log.info("admin login................");
-		String map = ad.login(mem.getId(), mem.getPw());
-		
-		String url = null;
-		
-		if(map.equals("0")){
-			url = "/ad_main/m_main";
-			sess.setAttribute("adminid", mem.getId());
-		} else if(map.equals("1")) {
-			url = "/ad_main/m_login";
-		} else {
-			url = "/ad_main/m_login";
-		}
-		
-		return url;
-	}
 
-}	
 
+
+  @Controller
+  
+  public class AdminController {
+  
+	  private static final Logger log = org.slf4j.LoggerFactory.getLogger(AdminController.class);
+	  
+	  
+	  
+	  @GetMapping("/") 
+	  public String m_login() { 
+		  log.info("admin home...........");
+		  return "/ad_main/m_login"; 
+	  }
+	  @PostMapping("adminlogin")
+	  public String 
+  }
+ 
 
